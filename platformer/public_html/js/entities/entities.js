08f4726cae61c5e8bb488893255587782913ac28
sebastianojeda <sebastianojeda.school@gmail.com>
2014-04-08 21:01:14 -0700

    Initial Commit
    First time uploading my platformer game made lots of levels and put in
    tons of code to make my game work.

---
+++ platformer/public_html/js/entities/entities.js
@@ -0,0 +1,53 @@
+game.PlayerEntity = me.ObjectEntity.extend({
+    init: function(x,y,settings){
+        settings.image = "player1-spritesheet";
+        settings.spritewidth = "72";
+        settings.spriteheight = "97";
+        this.parent(x,y,settings);
+        
+        this.collidable = true;
+        
+        this.renderable.addAnimation("idle", [3]); 
+        this.renderable.setCurrentAnimation("idle");
+        
+        this.setVelocity(5, 20);
+        
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH); 
+    },
+       
+    update: function(){
+        if(me.input.isKeyPressed("right")){
+            this.vel.x += this.accel.x * me.timer.tick;
+        }
+        else if(me.input.isKeyPressed("left")){
+            this.vel.x -= this.accel.x * me.timer.tick;
+        }
+        
+         else if(me.input.isKeyPressed("up")){
+            this.doJump();
+        }
+        
+             else{
+            this.vel.x =0;
+        }
+        
+        var collision = this.collide();
+        this.updateMovement();
+        return true;
+    }
+});
+
+game.LevelTrigger = me.ObjectEntity.extend({
+    init: function(x, y, settings){
+       this.parent(x, y, settings); 
+       this.collidable = true;
+       this.level = settings.level;
+    },
+            
+    onCollision: function(){
+       this.collidable = false;
+       me.levelDirector.loadLevel.defer(this.level);
+       me.state.current().resetPlayer.defer();
+    }
+    
+});
---
+++ platformer/public_html/js/resources.js
@@ -0,0 +1,38 @@
+game.resources = [
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+
+           {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+           {name: "player1-spritesheet", type:"image", src: "data/img/player1-spritesheet.png"},
+           {name: "solid-tile", type:"image", src: "data/img/solid-tile.png"},
+           {name: "Space-Universe", type:"image", src: "data/img/Space-Universe.jpg"},
+           {name: "cloud-tiles", type:"image", src: "data/img/cloud-tiles.png"},
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+        
+           {name: "level01", type:"tmx", src: "data/map/level01.tmx"},
+           {name: "level02", type:"tmx", src: "data/map/level02.tmx"},
+           {name: "level03", type:"tmx", src: "data/map/level03.tmx"},
+           {name: "level04", type:"tmx", src: "data/map/level04.tmx"}
+
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/", channel : 1},
+	 */	
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/", channel : 2}
+	 */
+];
